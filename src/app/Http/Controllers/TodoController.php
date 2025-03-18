ba376ade7def698d97ea7b5bb67ce9f3a4ef53f4
yuuya suzuki <szk1613y@gmail.com>
2025-03-18 23:34:28 +0900

    add,feat:詳細表示機能,ページの追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -15,10 +15,12 @@ class TodoController extends Controller
         return view('todo.index', ['todos' => $todos]);
     }
     
+
     public function create()
     {
         return view('todo.create');
     }
+
 
     public function store(Request $request)
     {
@@ -29,5 +31,14 @@ class TodoController extends Controller
         $todo->save();
 
         return redirect()->route('todo.index');
+    }
+
+
+    public function show($id)
+    {
+        $model = new Todo();
+        $todo = $model->find($id);
+        
+        return view('todo.show', ['todo' => $todo]);
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -19,3 +19,6 @@ Route::get('/todo', 'TodoController@index')->name('todo.index');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 
 Route::post('/todo', 'TodoController@store')->name('todo.store');
+
+// 詳細表示
+Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
