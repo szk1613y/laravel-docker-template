0949a6fa138353adea80a5f22efcbcc51cdfa693
yuuya suzuki <szk1613y@gmail.com>
2025-03-18 23:58:54 +0900

    refactor:Todocontrollerのモデルインスタンスのリファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,10 +7,17 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
+
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        $todos = $this->todo->all();
 
         return view('todo.index', ['todos' => $todos]);
     }
@@ -26,9 +33,8 @@ class TodoController extends Controller
     {
         $inputs = $request->all();
 
-        $todo = new Todo();
-        $todo->fill($inputs);
-        $todo->save();
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
         return redirect()->route('todo.index');
     }
@@ -36,9 +42,8 @@ class TodoController extends Controller
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
-        
+        $todo = $this->todo->find($id);
+
         return view('todo.show', ['todo' => $todo]);
     }
 }
