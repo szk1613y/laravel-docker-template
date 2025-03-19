7fd3cbbafbcd059d405e32f0dbd89df938282000
yuuya suzuki <szk1613y@gmail.com>
2025-03-20 00:14:19 +0900

    feat:ToDo削除機能の実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -26,3 +26,6 @@ Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
 // 編集機能
 Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
 Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
+
+// 削除機能
+Route::delete('/todo/{id}', 'TodoController@delete')->name('todo.delete');
